749ce6138c4ecfd25e95371a44894c9d45543dad
igancio108 <botellalledin@gamilcom>
2025-11-15 23:59:26 +0000

    ssh service script done

---
+++ scripts/security-access-auth.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+#Install an enable ssh server
+
+#apt install openssh-server
+
+systemctl enable ssh
+
+
+#Disable password login and enable only authentication via asymetric keys
+
+echo "PasswordAuthentication no" >> /etc/ssh/ssh_config
+echo "PubKeyAuthentication yes" >> /etc/ssh/ssh_config
+
+#Use an alternative port, port 2222
+echo "Port 2222" >> /etc/ssh/ssh_config
+
+#Allow the users we created to use the ssh service
+echo "AllowUsers Vicent_bare Jules_fedit Ignacio_botella" >> /etc/ssh/ssh_config
+
+#Create an custom banner
+
+touch /etc/ssh/ssh_config.d/custom_banner
+
+echo "
+    *********************************************
+    *Authorized access only                     *
+    *                                           *
+    *If you are not Vincent, Jules or Ignacio.  *
+    *Please disconnect INMEDIATLY               *
+    *********************************************" >> /etc/ssh/ssh_config.d/custom_banner
+
+
+
+echo "Banner /etc/ssh/ssh_config.d/custom_banner" >> /etc/ssh/ssh_config
+
+#Harden Cyphers and cryptographics algorithms 
+
+echo "Ciphers aes256-ctr,aes192-ctr,aes128-ctr" >> /etc/ssh/ssh_config
+echo "MACs hmac-sha512,hmach-sha256" >> /etc/ssh/ssh_config
+echo "KexAlgorithms curve25519-sha256,diffie-hellman-group-exchange-sha256" >> /etc/ssh/ssh_config
+
+#Resatart ssh service
+
+systemctl restart ssh
+
+#Check configuration
+sshd -T | grep ciphers
+sshd -T | grep macs
+sshd -T | grep kexalgorithms
+sshd -T | grep port
+sshd -T | grep allowusers
+
+sleep 60
+
+ss -tlnp | grep sshd
