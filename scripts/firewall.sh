f22e7595c7ac115cfd1117307a69f48c51fa92c2
igancio108 <botellalledin@gamilcom>
2025-11-16 15:54:32 +0000

    firewall_fail2ban script done

---
+++ scripts/firewall.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+apt install ufw -y
+apt install fail2ban -y
+
+
+ufw allow 22/tcp
+ufw allow http
+ufw allow https
+ufw allow 2222/tcp
+
+
+
+
+ufw default deny incoming
+
+ufw default deny outgoing
+
+#Activate ufw to be persistant
+
+ufw enable
+
+#Activate logging
+
+ufw logging on
+
+ufw status verbose
+
+#Fail2ban
+
+touch /etc/fail2ban/jail.d/sshd.local
+
+
+echo "[sshd]" >> /etc/fail2ban/jail.d/sshd.local
+echo "enabled = true" >> /etc/fail2ban/jail.d/sshd.local
+echo "port = 2222" >> /etc/fail2ban/jail.d/sshd.local
+echo "maxretry = 3" >> /etc/fail2ban/jail.d/sshd.local
+echo "bantime = 10m" >> /etc/fail2ban/jail.d/sshd.local
+echo "logpath = /var/log/fail2ban.log" >> /etc/fail2ban/jail.d/sshd.local
+
+systemctl restart fail2ban
+systemctl enable fail2ban
+
+fail2ban-client status sshd
